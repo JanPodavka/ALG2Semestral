4902422014f397697b766ecce1dfe0d78925ff01
Jan Podávka <jan.podavka@tul.cz>
2021-05-07 20:05:31 +0200

    test

---
+++ FilmsAndSeries/src/app/Testing.java
@@ -0,0 +1,18 @@
+package app;
+
+
+
+public class Testing {
+
+
+    /*
+        doc
+        @Jan Podávka
+        https://github.com/JanPodavka
+        */
+    public static void main(String[] args) {
+
+       // String NetflixPath =
+        
+    }
+}
