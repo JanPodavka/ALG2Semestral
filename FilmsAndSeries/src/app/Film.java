4ce51842177bda3face03be918cf557f82e369b3
Jan Podávka <jan.podavka@tul.cz>
2021-05-08 19:06:34 +0200

    Succesfull reading of file and Films.java creation

--- FilmsAndSeries/src/app/Film.java
+++ FilmsAndSeries/src/app/Film.java
@@ -7,15 +7,42 @@ public class Film implements Watching {
    https://github.com/JanPodavka
    */
 //Data
-    String name;
-    boolean watched;
-    int stars;
+    private String title;
+    private boolean watched;
+    private int rating;
+    private String duration;
+    private String category;
+    private int index;
+    private String genre;
+//constructors
 
+    public Film(){
+    }
+
+    public Film(String title, boolean watched, int rating, String duration, String category,int index,String genre) {
+        this.title = title;
+        this.watched = watched;
+        this.rating = rating;
+        this.duration = duration;
+        this.category = category;
+        this.index = index;
+        this.genre = genre;
+    }
 
     @Override
     public String getName() {
         return null;
     }
 
-
+    @Override
+    public String toString() {
+        return "Film{" +
+                "title='" + title + '\'' +
+                ", watched=" + watched +
+                ", rating=" + rating +
+                ", duration='" + duration + '\'' +
+                ", category='" + category + '\'' +
+                ", index=" + index +
+                ", genre=" + genre +"\n \n";
+    }
 }
--- FilmsAndSeries/src/app/Testing.java
+++ FilmsAndSeries/src/app/Testing.java
@@ -1,6 +1,10 @@
 package app;
 
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class Testing {
 
@@ -10,9 +14,41 @@ public class Testing {
         @Jan Podávka
         https://github.com/JanPodavka
         */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//čtení z netflix databaze -init database
+        String path = "//C:/Users/janpo/Tul_alg/ALG2Semestral/FilmsAndSeries/src/utils/netflix_titles.csv";
+        File netflixDTB = new File(path);
+        BufferedReader nfxDTB = new BufferedReader(new FileReader(netflixDTB));
+        System.out.println(nfxDTB.readLine() +"\n" + nfxDTB.readLine());
+        //show_id	type	title	date_added	release_year	duration	listed_in
 
-       // String NetflixPath =
-        
+
+        //
+        ArrayList<Film> films = new ArrayList<>();
+//Úvodní zápis a tvorba objektů - 2. metoda
+        String line;
+        int index = 1;
+        String category;
+        String title;
+        String duration;
+        String genre;
+        Film add;
+        while ((line = nfxDTB.readLine()) != null){
+            String[] parts = line.split(";");
+            category = parts[1];
+            title = parts[2];
+            duration = parts[5];
+            genre = parts[6];
+            //System.out.println(index + " " + category + " " + title + " " + duration + " " + genre);
+            index++;
+            add = new Film(title,false,0,duration,category,index,genre);
+            films.add(add);
+        }
+        System.out.println("Test objektu");
+        System.out.println(films); //films = všechny vytvořené films z netflixu
+
+
+
+
     }
 }
