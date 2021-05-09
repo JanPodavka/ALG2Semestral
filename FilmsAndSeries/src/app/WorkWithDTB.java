7b11e9b0a3d8eb5e18c765d8f106c7ee583ebc8a
Jan Podávka <jan.podavka@tul.cz>
2021-05-09 02:52:45 +0200

    init - methods for working with files

---
+++ FilmsAndSeries/src/app/WorkWithDTB.java
@@ -0,0 +1,10 @@
+package app;
+
+public class WorkWithDTB {
+    /*
+    doc
+    @Jan Podávka
+    https://github.com/JanPodavka
+    */
+
+}
---
+++ FilmsAndSeries/src/ui/TesingUI.java
@@ -0,0 +1,11 @@
+package ui;
+
+public class TesingUI {
+    /*
+    doc
+    @Jan Podávka
+    https://github.com/JanPodavka
+    */
+    public static void main(String[] args) {
+    }
+}
