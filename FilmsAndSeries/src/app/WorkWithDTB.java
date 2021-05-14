a36ae06ef01cddf46f5a0492c56922010f4e789e
Jan Podávka <jan.podavka@tul.cz>
2021-05-14 04:11:41 +0200

    Reading file from netflixDTB and from OwnDtb and make object

--- FilmsAndSeries/src/app/WorkWithDTB.java
+++ FilmsAndSeries/src/app/WorkWithDTB.java
@@ -19,7 +19,7 @@ public class WorkWithDTB {
      * show_id	type	title	date_added	release_year	duration	listed_in
      * delimiter is ;
      */
-    public static ArrayList<Watching> initDTB(String path) {
+    public static ArrayList<Watching> initDTB(String path, boolean ownDTB) {
         //buffer init
         try {
             File netflixDTB = new File(path);
@@ -32,20 +32,30 @@ public class WorkWithDTB {
             String title;
             int duration;
             String genre;
+            boolean watched = false;
+            int rating = 0;
             Watching add;
             br.readLine(); //skip the first line
             //Objects making
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(";");
-                category = parts[1];
-                title = parts[2];
-                duration = Integer.parseInt(parts[5].split(" ")[0]);
-                genre = parts[6];
-
+                if (!ownDTB) { //if it is netflix DTB
+                    category = parts[1];
+                    title = parts[2];
+                    duration = Integer.parseInt(parts[5].split(" ")[0]);
+                    genre = parts[6];
+                } else { //if myDTB already exist ------------Mabey split in the methods ?
+                    category = parts[1];
+                    title = parts[2];
+                    watched = Boolean.parseBoolean(parts[3]);
+                    rating = Integer.parseInt(parts[4]);
+                    duration = Integer.parseInt(parts[5]);
+                    genre = parts[6];
+                }
                 if (Objects.equals(category, "Movie")) { //if it is a movie
-                    add = new Film(title, false, 0, duration, index, genre);
+                    add = new Film(title, watched, rating, duration, index, genre);
                 } else { //if it is a TV show
-                    add = new TVShows(title, false, 0, duration, index, genre);
+                    add = new TVShows(title, watched, rating, duration, index, genre);
                 }
                 index++;
                 films.add(add);
@@ -63,10 +73,10 @@ public class WorkWithDTB {
         BufferedWriter bw = new BufferedWriter(
                 new FileWriter("//C:/Users/janpo/Tul_alg/ALG2Semestral/FilmsAndSeries/src/utils/MyDTB.csv"));
         bw.write("index;name;watched;rating;duration;genre\n");
-        for (Watching film:seznam) {
-            String isMovie = (film instanceof Film?"Movie":"TV show"); //Instance of Film or TVshow ?
-            bw.write(  film.getIndex() + ";" + isMovie + ";" + film.getName() + ";" + film.getWatched()+ ";" +
-                    ";" + film.getRating()+ ";" + film.getDuration() + ";"+ film.getGenre() +"\n");
+        for (Watching film : seznam) {
+            String isMovie = (film instanceof Film ? "Movie" : "TV show"); //Instance of Film or TVshow ?
+            bw.write(film.getIndex() + ";" + isMovie + ";" + film.getName() + ";" + film.getWatched() + ";"
+                    + film.getRating() + ";" + film.getDuration() + ";" + film.getGenre() + "\n");
         }
         bw.close();
 
--- FilmsAndSeries/src/ui/TestingUI.java
+++ FilmsAndSeries/src/ui/TestingUI.java
@@ -14,15 +14,18 @@ public class TestingUI {
     https://github.com/JanPodavka
     */
     public static void main(String[] args) throws IOException {
-        String path = "//C:/Users/janpo/Tul_alg/ALG2Semestral/FilmsAndSeries/src/utils/netflix_titles.csv";
-        ArrayList<Watching> films = WorkWithDTB.initDTB(path); //načtení BR
-        Film film1 = new Film("Test Film",true,5,150,1,"střílečka");
+        //pokud ještě nemám svoji databázi
+        /*String path = "//C:/Users/janpo/Tul_alg/ALG2Semestral/FilmsAndSeries/src/utils/netflix_titles.csv";
+        ArrayList<Watching> films = WorkWithDTB.initDTB(path,false); //načtení BR
+        Film film1 = new Film("Test Film",true,5,150, films.size()+1, "střílečka");
         assert films != null;
         films.add(film1);
-        TVShows tvs = new TVShows("Peaky Blinders",true,10,5,2,"Epiiic");
+        TVShows tvs = new TVShows("Peaky Blinders",true,10,5,films.size()+1,"Epiiic");
         films.add(tvs);
-        WorkWithDTB.saveToMyDTB(films);
+        WorkWithDTB.saveToMyDTB(films);*/
 
-
+        //pokud již mám svoji databázi
+        ArrayList<Watching> films = WorkWithDTB.initDTB("//C:/Users/janpo/Tul_alg/ALG2Semestral/FilmsAndSeries/src/utils/MyDTB.csv",true);
+        System.out.println(films);
     }
 }
