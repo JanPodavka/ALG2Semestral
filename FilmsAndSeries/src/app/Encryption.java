2cc999f38c171f7a8d86b7932d03c9cf141e938a
Jan Podávka <jan.podavka@tul.cz>
2021-05-26 07:40:08 +0200

    encription ready to implement

--- FilmsAndSeries/src/app/Encryption.java
+++ FilmsAndSeries/src/app/Encryption.java
@@ -1,10 +1,74 @@
 package app;
 
+
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+
 public class Encryption {
-    /*
-    doc
-    @Jan Podávka
-    https://github.com/JanPodavka
-    */
+
+public static final String key = "TajnyklickmojiDB"; // klič k DTB [84, 97, 106, 110, 121, 107, 108, 105, 99, 107, 109, 111, 106, 105, 68, 66]
+
+    /**
+     * @param password String password
+     * @param cipher cipher
+     * @param aesKey key
+     * @return byte array of values 
+     * @throws InvalidKeyException
+     * @throws NoSuchAlgorithmException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     * @throws NoSuchPaddingException
+     */
+    public static byte[] encrypt(String password,Cipher cipher,Key aesKey) throws InvalidKeyException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException {
+
+        cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+        return cipher.doFinal(password.getBytes());
+        //new String(encrypted); použít pro čtení
+    }
+
+    /**
+     * @param enryptedPSWD ciphered pswd
+     * @param aesKey aeskey
+     * @param cipher cipher
+     * @return take array of ciphered password and transform
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     */
+    public static String decrypt(byte[] enryptedPSWD,Key aesKey,Cipher cipher) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        cipher.init(Cipher.DECRYPT_MODE, aesKey);
+        String decrypted = new String(cipher.doFinal(enryptedPSWD));
+        return decrypted;
+   }
+
+    public static void main(String[] args) throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        Key aesKey = new SecretKeySpec(key.getBytes(), "AES"); // na 128bit code
+        Cipher cipher = Cipher.getInstance("AES"); //vytvoří instanci cipher
+        byte[] secret = encrypt("Honza",cipher,aesKey);
+        String secredString = new String(secret);
+        String normal = decrypt(secret,aesKey,cipher);
+        //String normal = decrypt("��עix��",cipherkey,descipher);
+        System.out.println(Arrays.toString(secret));
+        System.out.println();
+        System.out.println(normal);
+
+
+
+
+    }
+
+
 
 }
+
